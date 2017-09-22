21ae8e9b7f01618d7603b5f8363fbe1641e1f1b4
wanxin0001 <wanxin00001@gmail.com>
2017-09-22 00:33:32 -0700

    Add updateItem samples

--- src/MoviesItemOps.java
+++ src/MoviesItemOps.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,11 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 
 
 public class MoviesItemOps {
@@ -20,6 +26,9 @@ public class MoviesItemOps {
 		Table table = dynamoDB.getTable("Movies");
 		
 		putItemSample(table);
+		getItemSample(table);
+		updateItemSample(table);
+		incrementAtomicCounterSample(table);
 	}
 	
 	public static void putItemSample(Table table) {
@@ -39,6 +48,59 @@ public class MoviesItemOps {
 			System.out.println("Unable to add Item. ErrMsg: " + e.getMessage());
 		}
 		
+	}
+	
+	public static void getItemSample(Table table) {
+		int year = 2015;
+		String title = "The Big New Moive";
+		
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey("year", year, "title", title);
+		try {
+			System.out.println("Attempt to read the item... ");
+			Item outcome = table.getItem(spec);
+			System.out.println("GetItem succeeded:" + outcome);
+		} catch (Exception e) {
+			System.out.println("Unable to read item: " + year + " " + title);
+			System.out.println("ErrorMessage: " + e.getMessage());
+		}
+	}
+	
+	public static void updateItemSample(Table table) {
+		int year = 2015;
+		String title = "The Big New Moive";
+		
+		UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("year", year, "title", title)
+				.withUpdateExpression("set info.rating = :r, info.plot1 = :p, info.actors = :a")
+				.withValueMap(new ValueMap().withNumber(":r", 5.5)
+						.withString(":p", "Everything happens all at once.")
+						.withList(":a", Arrays.asList("Larry", "Moe", "Curly")))
+				.withReturnValues(ReturnValue.UPDATED_NEW);
+		
+		try {
+			System.out.println("Updating the item ...");
+			UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+			System.out.println("UpdateItem succeeded: " + outcome);
+		} catch (Exception e) {
+			System.out.println("Unable to update item. ErrMsg: " + e.getMessage());
+		}
+	}
+	
+	public static void incrementAtomicCounterSample(Table table) {
+		int year = 2015;
+		String title = "The Big New Moive";
+		
+		UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("year", year, "title", title)
+				.withUpdateExpression("set info.rating = info.rating + :val, info1 = :val")
+				.withValueMap(new ValueMap().withNumber(":val", 1))
+				.withReturnValues(ReturnValue.ALL_NEW);
+		
+		try {
+			System.out.println("Updating the item ...");
+			UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+			System.out.println("UpdateItem succeeded: " + outcome.getItem().toJSONPretty());
+		} catch (Exception e) {
+			System.out.println("Unable to update item. ErrMsg: " + e.getMessage());
+		}
 	}
 
 }
